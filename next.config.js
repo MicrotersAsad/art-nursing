b6f12d4ea6285ada7468c39c80fded5933c37214
MicrotersAsad <sheblumicroters@gmail.com>
2024-10-02 13:33:08 +0600

    add

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',  // For Azure deployment
   reactStrictMode: true,
   images: {
-    unoptimized: true,  // Disables image optimization for static exports
+    unoptimized: true,  // Disables image optimization if you're using Azure's CDN or Blob Storage for static images
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/uploads/**',
+        protocol: 'https',  // Update to 'https' for production, assuming your app uses SSL
+        hostname: 'https://art-nursing-cvh4bkh8bra9b5ab.eastus-01.azurewebsites.net/',  // Replace with your production hostname or CDN
+        port: '',  // Leave empty unless you're specifying a port in production
+        pathname: '/uploads/**',  // Ensure this matches your production URL structure for image storage
       },
     ],
   },
- 
 };
+
+module.exports = nextConfig;
