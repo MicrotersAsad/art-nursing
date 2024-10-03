b505f4964dc0a240b9c3256514831046ea4287a1
MicrotersAsad <sheblumicroters@gmail.com>
2024-10-03 15:28:35 +0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import { createServer } from "http";
-import { parse } from "url";
-import next from "next";
+const http = require("http");
+const url = require("url");
+const next = require("next");
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -8,14 +8,14 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);  // Corrected this line
+  http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);  // Using 'url' with 'require'
     handle(req, res, parsedUrl);
   }).listen(port);
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? "development" : process.env.NODE_ENV
-    }`,
+    }`
   );
 });
